ad73a8e0d4cd6489e244654db48060f2247e2539
tidypr <tidypr@icloud.com>
2025-04-15 11:40:12 +0900

    chore: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common'; // ValidationPipe를 사용하여 DTO 검증을 수행
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // SwaggerModule을 사용하여 Swagger 문서화
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,18 @@ async function bootstrap() {
       whitelist: true, // DTO에 정의되지 않은 속성 제거
     }),
   );
+
+  // swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Game Server')
+    .setDescription('Game Server API description')
+    .setVersion('1.0')
+    .addTag('game')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // run server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
